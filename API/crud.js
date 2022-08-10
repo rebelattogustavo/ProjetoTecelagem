9f057ab67506699d18ac0d495aa89ff8248552b9
João <91199065+NIhilth@users.noreply.github.com>
2022-08-10 19:27:56 -0300

    crud feito

--- API/crud.js
+++ API/crud.js
@@ -12,4 +12,66 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+async function save(nomeTabela, id, dado) {
+  if (id) {
+      const entidadeReferencia = await setDoc(doc(db, nomeTabela, id), dado);
+      const dataSalva = {
+          ...dado,
+          id: id
+      }
+      return dataSalva;
+  } else {
+      const entidadeReferencia = await addDoc(collection(db, nomeTabela), dado);
+      const dataSalva = {
+          ...dado,
+          id: entidadeReferencia.id
+      }
+      return dataSalva;
+  }
+}
+
+async function get(nomeTabela) {
+  const tabelaRef = collection(db, nomeTabela);
+
+  const q = query(tabelaRef);
+  
+  const querySnapshot = await getDocs(q);
+  
+  const lista = [];
+  querySnapshot.forEach((doc) => {
+      const dado = {
+          ...doc.data(),
+          id: doc.id
+      }
+      lista.push(dado);
+  });
+  return lista;
+}
+
+async function getById(nomeTabela, id) {
+  const docRef = doc(db, nomeTabela, id);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+      return docSnap.data();
+  } else {
+      return new Error("Not found!");
+  }
+
+}
+
+async function remove(nomeTabela, id){
+  const dado = await deleteDoc(doc(db, nomeTabela, id));
+  return {
+      message: `${id} deleted`
+  }
+}
+
+module.exports = {
+  save,
+  get,
+  getById,
+  remove
+}
