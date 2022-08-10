35b0f9b1934497df8f5aed20d777283ef33c87c8
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-08-10 20:02:00 -0300

    rotas funcionando

---
+++ APP/src/app/fio/fio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fio',
+  templateUrl: './fio.component.html',
+  styleUrls: ['./fio.component.css']
+})
+export class FioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ APP/src/app/malha/malha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-malha',
+  templateUrl: './malha.component.html',
+  styleUrls: ['./malha.component.css']
+})
+export class MalhaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
