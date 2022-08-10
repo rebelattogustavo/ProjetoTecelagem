add102c30256b2a0298a59bcd27db6fdab5a31ff
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-08-10 20:25:29 -0300

    10/08 - 20:25

--- APP/src/app/app.module.ts
+++ APP/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RotasModule } from './rotas.module';
@@ -12,7 +13,8 @@ import { TelaLoginComponent } from './tela-login/tela-login.component';
   ],
   imports: [
     BrowserModule,
-    RotasModule
+    RotasModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
