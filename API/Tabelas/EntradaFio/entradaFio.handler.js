3073700d3e0bfdbe5ef9e4eb6913793bb4632134
João <91199065+NIhilth@users.noreply.github.com>
2022-08-10 21:41:12 -0300

    alguns handlers finalizados

---
+++ API/Tabelas/EntradaFio/entradaFio.handler.js
@@ -0,0 +1,33 @@
+const crud = require("../../crud");
+
+const cadastrarEntradaFio = async (qtdCaixa, peso, rolosPorCaixa, precoNotaFiscal, clienteId, notaFiscalId, fornecedorId, fioId, id) => {
+    let entradaFio;
+    if (id) {
+        entradaFio = await crud.cadastrar("entrada-fio", id, { qtdCaixa, peso, rolosPorCaixa, precoNotaFiscal, clienteId, notaFiscalId, fornecedorId, fioId });
+    } else {
+        entradaFio = await crud.cadastrar("entrada-fio", null, { qtdCaixa, peso, rolosPorCaixa, precoNotaFiscal, clienteId, notaFiscalId, fornecedorId, fioId });
+    }
+    return entradaFio;
+}
+
+const removerEntradaFio = async (id) => {
+    crud.remover("entrada-fio", id);
+    return buscarEntradaFios();
+}
+
+const buscarEntradaFios= async () => {
+    const listaEntradaFios = await crud.buscar("entrada-fio");
+    return listaEntradaFios;
+}
+
+const buscarEntradaMaterialId = async (id) => {
+    const entradaFio = await crud.buscarPorId("entrada-fio", id);
+    return entradaFio;
+}
+
+module.exports = {
+    cadastrarEntradaFio,
+    removerEntradaFio,
+    buscarEntradaFios,
+    buscarEntradaMaterialId
+}
