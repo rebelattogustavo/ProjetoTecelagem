b3e54641f5dc0d417a17bf1a772754405a65e5ac
rebelattogustavo <gu.zapella@gmail.com>
2022-08-31 20:51:53 -0300

    VAI SE FUDE FRONT-END

--- API/Tabelas/FioMalha/fioMalha.handler.js
+++ API/Tabelas/FioMalha/fioMalha.handler.js
@@ -2,32 +2,26 @@ const crud = require("../../crud");
 
 const cadastrarFioMalha = async (fioid, malhaId, id) => {
     let fioMalhas;
-<<<<<<< HEAD
-    const fio  =  await crud.buscarPorId("fio", toString(fioid));
+    const fio = await crud.buscarPorId("fio", toString(fioid));
     const malha = await crud.buscarPorId("malha", toString(malhaId));
-    if(!fio.naoEncontrado || !malha.naoEncontrado){
-=======
-    const fio  =  await crud.buscarPorId("fio", fioid);
-    const malha = await crud.buscarPorId("malha", malhaId);
-    console.log("fio", fio);
-    console.log("malha", malha);
+    if (!fio.naoEncontrado || !malha.naoEncontrado) {
+        const fio = await crud.buscarPorId("fio", fioid);
+        const malha = await crud.buscarPorId("malha", malhaId);
+        console.log("fio", fio);
+        console.log("malha", malha);
 
-    if(!fio.naoEncontrado && !malha.naoEncontrado){
->>>>>>> dbbfeca44c2be1968372e8695d0607211e73d028
-        if (id) {
-            fioMalhas = await crud.cadastrar("fio-malha", id, { fioid, malhaId });
+        if (!fio.naoEncontrado && !malha.naoEncontrado) {
+            if (id) {
+                fioMalhas = await crud.cadastrar("fio-malha", id, { fioid, malhaId });
+            } else {
+                fioMalhas = await crud.cadastrar("fio-malha", null, { fioid, malhaId });
+            }
         } else {
-            fioMalhas = await crud.cadastrar("fio-malha", null, { fioid, malhaId });
-        } 
-    } else {
-        return { "Erro": "Fio e/ou Malha não existem!" };
+            return { "Erro": "Fio e/ou Malha não existem!" };
+        }
+
+        return fioMalhas;
     }
-
-<<<<<<< HEAD
-
-=======
->>>>>>> dbbfeca44c2be1968372e8695d0607211e73d028
-    return fioMalhas;
 }
 
 const removerFioMalha = async (id) => {
--- API/Tabelas/Malha/malha.handler.js
+++ API/Tabelas/Malha/malha.handler.js
@@ -9,41 +9,32 @@ const cadastrarMalha = async (descricao, valor, id) => {
             return { "Erro": "Malha inexistente" };
         }
 
-<<<<<<< HEAD
-        malha = await crud.cadastrar("malha", id, {descricao});
-=======
-        malha = await crud.cadastrar("malha", id, {descricao, valor});
->>>>>>> dbbfeca44c2be1968372e8695d0607211e73d028
+        malha = await crud.cadastrar("malha", id, { descricao });
+        malha = await crud.cadastrar("malha", id, { descricao, valor });
     } else {
-        malha = await crud.cadastrar("malha", null, {descricao,valor});
+        malha = await crud.cadastrar("malha", null, { descricao, valor });
     }
     return malha;
 }
 
 const removerMalha = async (id) => {
     const malha = await buscarMalhaId(id);
-<<<<<<< HEAD
 
     if (!malha.naoEncontrado) {
-=======
-    const fioMalha = await crud.buscar("fio-malha");
+        const fioMalha = await crud.buscar("fio-malha");
 
-    if (!malha.naoEncontrado) {
-        for(let fMalha of fioMalha){
-            if(malha.id == fMalha.malha_id){
-                const objDeletado = await crud.remover("fio-malha", fMalha.id);
+        if (!malha.naoEncontrado) {
+            for (let fMalha of fioMalha) {
+                if (malha.id == fMalha.malha_id) {
+                    const objDeletado = await crud.remover("fio-malha", fMalha.id);
+                }
             }
+            await crud.remover("malha", id);
+        } else {
+            return { "Erro": "Malha inexistente" };
         }
->>>>>>> dbbfeca44c2be1968372e8695d0607211e73d028
-        await crud.remover("malha", id);
-    } else {
-        return { "Erro": "Malha inexistente" };
+        return { "Sucesso": `Malha removida com sucesso!` };
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> dbbfeca44c2be1968372e8695d0607211e73d028
-    return { "Sucesso": `Malha removida com sucesso!` };
 }
 
 const buscarMalhas = async () => {
