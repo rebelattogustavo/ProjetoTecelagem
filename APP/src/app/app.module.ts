8a1db18ec4f4d7789f2821076c1dc52c85cfb4e0
VINICIUS BONATTI BENNER <vinicius_benner@estudante.sesisenai.org.br>
2022-09-02 19:40:29 -0300

    ajeitando erro

--- APP/src/app/app.module.ts
+++ APP/src/app/app.module.ts
@@ -23,13 +23,10 @@ import { HistoricoProducaoComponent } from './historico-producao/historico-produ
 import { SacolaComponent } from './sacola/sacola.component';
 
 import { HttpClientModule } from  '@angular/common/http';
-<<<<<<< HEAD
 import { CadastroFioComponent } from './cadastroFio/cadastroFio.component';
 import { cadastroItemComponent } from './cadastroItem/cadastroItem.component';
 import { CadastroMalhaComponent } from './cadastroMalha/cadastroMalha.component';
-=======
 import CheckLogged from './checklogged';
->>>>>>> 102f61f0601ca95eb52eb06f418ab47244c3f770
 
 @NgModule({
   declarations: [
